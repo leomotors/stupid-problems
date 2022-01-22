b0c7f7a680723d55bed76bb99dc2a84ddb2395ff
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2022-01-22 15:32:57 +0700

    Looks better with font

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Stupid Problems
 
 Problem Statements of Problems I wrote
+
+**Fun Fact**: I have not write Vanilla JS for quite a long time (9 months to be exact) since abandoned [Website Vector Calculator](https://github.com/Leomotors/Website-Vector-Calculator)
---
+++ script.js
@@ -0,0 +1,21 @@
+const questions = ["aplusb", "calculus_wtf", "cancel_1112", "nikkori_cafe"];
+
+const iframe = document.querySelector("iframe");
+const qToken = window.location.search.split("=");
+const question = window.location.search.split("=")[1];
+
+if (qToken.length >= 2) {
+  if (questions.indexOf(question) === -1) {
+    document.querySelector(".error").className += " activate";
+  } else {
+    iframe.src = `./${question}.pdf`;
+  }
+}
+
+const ul = document.querySelector(".problems-list");
+
+questions.forEach((question) => {
+  const li = document.createElement("li");
+  li.innerHTML = `<a href="./index.html?q=${question}">${question}</a>`;
+  ul.appendChild(li);
+});
