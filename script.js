75486c92b71f64d4ec83a88b64572ad03670d117
Leomotors (Ubuntu) <59821765+Leomotors@users.noreply.github.com>
2022-01-23 16:14:06 +0700

    Decorate and File Location

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-const questions = [
-  "aplusb",
-  "calculus_wtf",
-  "cancel_1112",
-  "nikkori_cafe",
-  "wtf",
-];
+const questions = {
+  aplusb: "A + B",
+  calculus_wtf: "Calculus WTF",
+  cancel_1112: "ยกเลิก 1112",
+  nikkori_cafe: "จอมเวทแห่งคาเฟ่รอยยิ้ม (เวอร์ชั่นง่าย)",
+  wtf: "WTF",
+};
 
-const iframe = document.querySelector("iframe");
+const iframe = document.querySelector(".pdf-viewer > iframe");
 const qToken = window.location.search.split("=");
-const question = window.location.search.split("=")[1];
+const question = qToken[1];
 
 if (qToken.length >= 2) {
-  if (questions.indexOf(question) === -1) {
-    document.querySelector(".not-found").className += " activate";
+  if (questions[question]) {
+    iframe.src = `./src/${question}.pdf`;
   } else {
-    iframe.src = `./${question}.pdf`;
+    document.querySelector(".not-found").className += " activate";
   }
 }
 
+// * Default
+iframe.src = iframe.src || "./src/aplusb.pdf";
+
+document.querySelector(".pdf-viewer").className += " loaded";
+
 const ul = document.querySelector(".problems-list");
 
-questions.forEach((question) => {
+for (const [short, full] of Object.entries(questions)) {
   const li = document.createElement("li");
-  li.innerHTML = `<a href="./index.html?q=${question}">${question}</a>`;
+  li.innerHTML = `<a href="?=${short}">${short} | ${full}</a>`;
   ul.appendChild(li);
-});
+}
